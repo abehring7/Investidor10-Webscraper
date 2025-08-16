24245d9504943407cc4e4b81f27cc9713347bd75
abehring <behringarthur@gmail.com>
2025-08-16 14:09:06 -0300

    Melhora no código e em sua documentação

--- Investidor10_div_scraper.py
+++ Investidor10_div_scraper.py
@@ -15,9 +15,9 @@ driver = webdriver.Firefox(service=service, options=options)
 actions = ActionChains(driver)
 wait = WebDriverWait(driver, 15)
 
-dfs = [] ## onde vai ficar os dfs
+dfs = [] ## onde vai ficar os dados
 
-def qtd_botoes():
+def qtd_botoes(): ## pega o ultimo botão da DOM do site, extraindo assim, a quantidade de botões.
     sel_botao = driver.find_element(
         By.CSS_SELECTOR, "a[data-dt-idx='6']"
     )
@@ -46,12 +46,11 @@ def formatacao_df():
     dfs_div = pd.DataFrame(dfs)
     dfs_div.columns = ["Proventos", "Data_COM", "Pagamento", "Valor Pago"]
     print(dfs_div)
-    return dfs_div
 
 def transformar_em_excel():
     while True:
         caminho = input("Digite o camiho e o nome do arquivo que quer salvar no PC: (exemplo: C:\\Users\\Nome\\Local\\arquivo): ")
-        caminho = caminho.replace("\\", "\\\\")
+        caminho = caminho.replace("\\", "\\\\") ## replace nas barras pro input do usuario ficar como raw string
         if not caminho:
             print("Caminho vazio arquivo nao salvo.")
             return
@@ -62,47 +61,46 @@ def transformar_em_excel():
             print(f"Salvo com sucesso em {caminho}")
             break
         except Exception as e:
-            print(f"Nao foi especificado o nome do arquivo, tente novamente{e}")
+            print(f"Nao foi especificado o nome do arquivo, tente novamente error: {e}")
 
 
 
-def clicar_localizar_next():
+def clicar_localizar_next(): ## Localiza e clica no botão "Próximo" para navegar entre as tabelas
     try:
-        localizador = (By.CSS_SELECTOR, "#table-dividends-history_next") ## trocar o botao pela paginacao next(tirar parametro)
-        botao = wait.until(EC.presence_of_element_located(localizador))
+        localizador = (By.CSS_SELECTOR, "#table-dividends-history_next") ## o botao "Proximo" por seletor CSS.
+        botao = wait.until(EC.presence_of_element_located(localizador))  ## espera ate o elemento estar presente no DOM
    
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-        driver.execute_script("""arguments[0].scrollIntoView({behavior:'instant', block:'center' });""", botao)
+        driver.execute_script("""arguments[0].scrollIntoView({behavior:'instant', block:'center' });""", botao) ## scrolla ate o botao estar visivel
         
 
-        driver.execute_script("arguments[0].click();", botao)
-        wait.until(EC.presence_of_element_located((By.ID, "table-dividends-history")))
+        driver.execute_script("arguments[0].click();", botao) ## clica no botao por JS
+        wait.until(EC.presence_of_element_located((By.ID, "table-dividends-history"))) ## espera ate a nova tabela de dividendos renderizar
         pegar_tabelas()
 
     except Exception as e:
-        print(f"nao foi possivel executar a funcao erro {e}")
+        print(f"Não foi possivel executar a funcao erro: {e}")
         return botao
 
 
 
 def pegar_tabelas():
     try:
-        tabela_div = driver.find_elements(By.ID, "table-dividends-history")
-        for i, tabelas in enumerate(tabela_div):
-            linhas = tabelas.find_elements(By.TAG_NAME, "tr")
-            for linha in linhas:
-                celulas = linha.find_elements(By.TAG_NAME, "td")
-                if celulas:
-                    dados = [col.text.strip() for col in celulas]
-                    dfs.append(dados)
+        tabela_div = driver.find_elements(By.ID, "table-dividends-history") ## acha a tabela de Dividendos
+        for i, tabelas in enumerate(tabela_div):                            ## itera sobre as (linhas, index) da tabela
+            linhas = tabelas.find_elements(By.TAG_NAME, "tr")               ## encontra as linhas tag="tr"
+            for linha in linhas:                                            ## itera sobre as celulas das linhas, os dados nas table row
+                celulas = linha.find_elements(By.TAG_NAME, "td")            ## encontra as celulas tag="td"
+                if celulas:                                                 ## caso celulas = True
+                    dados = [col.text.strip() for col in celulas]           ## formata os dados das celulas tirando os espaços em branco
+                    dfs.append(dados)                                       ## manda os dividendos da linha para a lista "dfs"
     except Exception as e:
         print(f"Falha ao buscar tabelas erro: {e}")
 
-## cria funcao para automatizar o next pelo num de botoes, com true e saindo no class disabled
 
 
-def automatizar_next():
+
+def automatizar_next(): ## cria funcao para automatizar o click no next pelo num de botoes
     botoes = qtd_botoes()
     for i in range(0, int(botoes)-2):
         clicar_localizar_next()
@@ -115,4 +113,3 @@ clicar_localizar_next() ## testar isso assim primeiro
 automatizar_next()
 formatacao_df()
 sel_excel()
-## organizar odf
